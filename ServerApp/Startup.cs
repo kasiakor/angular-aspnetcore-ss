84eb7e7724777501c0654a6b955b5862ac87af01
kasia.kordys <kasia.kordys@gmail.com>
2020-12-30 19:17:44 +0000

    register the context class in startup file

--- ServerApp/Startup.cs
+++ ServerApp/Startup.cs
@@ -6,9 +6,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ServerApp.Models;
 
 namespace ServerApp
 {
@@ -24,11 +26,14 @@ namespace ServerApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+            services.AddDbContext<DataContext>(options => options.UseSqlServer(connectionString));
+
             services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider services)
         {
             if (env.IsDevelopment())
             {
@@ -67,6 +72,8 @@ namespace ServerApp
                     spa.UseAngularCliServer("start");
                 }
             });
+
+            SeedData.SeedDatabase(services.GetRequiredService<DataContext>());
         }
     }
 }
