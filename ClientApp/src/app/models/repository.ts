807cc1f1423193a84c282b3eb431264378943a2f
kasia.kordys <kasia.kordys@gmail.com>
2021-01-05 22:32:31 +0000

    get web service data in the repository

--- ClientApp/src/app/models/repository.ts
+++ ClientApp/src/app/models/repository.ts
@@ -1,8 +1,15 @@
 import { Product } from "./product.model";
+import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
 
-export class Repository {
-  constructor() {
-    this.product = JSON.parse(document.getElementById("data").textContent);
+@Injectable()
+  export class Repository {
+      product: Product;
+    constructor(private http: HttpClient) {
+      this.getProduct(1);
+    }
+    getProduct(id: number) {
+      this.http.get<Product>("/api/products/" + id).subscribe(p => this.product = p);
+    }
   }
-  product: Product;  
-}
+
