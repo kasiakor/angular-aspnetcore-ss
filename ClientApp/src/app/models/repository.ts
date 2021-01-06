fbc51bbba24494cc987c01137978d4819b693d38
kasia.kordys <kasia.kordys@gmail.com>
2021-01-06 19:34:45 +0000

    add placeholder content when waiting for data in angular app

--- ClientApp/src/app/models/repository.ts
+++ ClientApp/src/app/models/repository.ts
@@ -4,12 +4,18 @@ import { HttpClient } from '@angular/common/http';
 
 @Injectable()
   export class Repository {
-      product: Product;
+      productData: Product;
     constructor(private http: HttpClient) {
       this.getProduct(1);
     }
     getProduct(id: number) {
-      this.http.get<Product>("/api/products/" + id).subscribe(p => this.product = p);
-    }
+      this.http.get<Product>("/api/products/" + id).subscribe(p => this.productData = p);
+      console.log("Product Data Received");
+  }
+
+  get product(): Product {
+    console.log("Product Data Requested");
+    return this.productData;
+  }
   }
 
