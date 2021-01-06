22f5d0629cbfb06608011ee2132816d85d361137
kasia.kordys <kasia.kordys@gmail.com>
2021-01-06 23:26:33 +0000

    break the circular reference setting navigation properties to null

--- ServerApp/Controllers/ProductValuesController.cs
+++ ServerApp/Controllers/ProductValuesController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ServerApp.Models;
+using System.Linq;
 
 namespace ServerApp.Controllers
 {
@@ -17,8 +19,54 @@ namespace ServerApp.Controllers
         [HttpGet("{id}")]
         public Product GetProduct(long id)
         {
-            System.Threading.Thread.Sleep(5000);
-            return context.Products.Find(id);
+            //System.Threading.Thread.Sleep(5000);
+            //DbContext.Find(type, Object[])
+            // return context.Products.Find(id);
+            //Find replaced by FirstOrDefault
+           Product result = context.Products.Include(p => p.Supplier).Include(p => p.Ratings).FirstOrDefault(p => p.ProductId == id);
+
+            if(result != null)
+            {
+                if(result.Supplier != null)
+                {
+                    result.Supplier.Products = null;
+                }
+                if(result.Ratings != null )
+                {
+                    foreach(Rating r in result.Ratings)
+                    {
+                        r.Product = null;
+                    }
+                }
+            }
+            return result;
         }
     }
 }
+
+//{
+//  "productId": 2,
+//  "name": "Lifejacket",
+//  "category": "Watersports",
+//  "description": "Protective and fashionable",
+//  "price": 48.95,
+//  "supplier": {
+//    "supplierId": 1,
+//    "name": "Splash Dudes",
+//    "city": "San Jose",
+//    "state": "CA",
+//    "products": null
+//  },
+//  "ratings": [
+//    {
+//      "ratingId": 3,
+//      "stars": 2,
+//      "product": null
+//    },
+//    {
+//      "ratingId": 4,
+//      "stars": 5,
+//      "product": null
+//    }
+//  ]
+//}
