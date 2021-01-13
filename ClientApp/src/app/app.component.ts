34f38a115bfe19de4b0486f51c23a30c394775dd
kasia.kordys <kasia.kordys@gmail.com>
2021-01-13 21:25:11 +0000

    add replace method in the component in angular app

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Repository } from "./models/repository";
 import { Product } from "./models/product.model";
+import { Supplier } from './models/supplier.model';
 
 @Component({
   selector: 'app-root',
@@ -27,5 +28,9 @@ export class AppComponent {
     p.name = "Modified Product";
     p.category = "Modified Category";
     this.repo.replaceProduct(p);
+  }
+  replaceSupplier() {
+    let s = new Supplier(3, "Modified Supplier", "New York", "NY");
+    this.repo.replaceSupplier(s);
   }
 }
