4c3ffcd4fdd7f302f63dd1607d29a8705b1ea223
kasia.kordys <kasia.kordys@gmail.com>
2021-01-15 19:37:49 +0000

    add upade product method in the component in angular app

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -33,4 +33,10 @@ export class AppComponent {
     let s = new Supplier(3, "Modified Supplier", "New York", "NY");
     this.repo.replaceSupplier(s);
   }
+  updateProduct() {
+    let changes = new Map<string, any>();
+    changes.set("name", "Green Kayak");
+    changes.set("supplier", null);
+    this.repo.updateProduct(1, changes);
+  }
 }
