1614a2d5be20c1098d2936590d05eb20cadd27bc
kasia.kordys <kasia.kordys@gmail.com>
2021-01-19 17:13:28 +0000

    add filter component in angular app

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ModelModule } from "./models/model.module";
+import { ProductTableComponent } from "./structure/productTable.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductTableComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ModelModule
+    ModelModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
