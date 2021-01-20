385558a2e9d4041adb5bb5643af52c8236c7b6a0
kasia.kordys <kasia.kordys@gmail.com>
2021-01-20 20:32:27 +0000

    add element to template to aplly new component in angular app

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { ModelModule } from "./models/model.module";
 import { ProductTableComponent } from "./structure/productTable.component";
 import { CategoryFilterComponent } from "./structure/categoryFilter.component";
+import { ProductDetailComponent } from "./structure/productDetail.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductTableComponent,
-    CategoryFilterComponent
+    CategoryFilterComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
