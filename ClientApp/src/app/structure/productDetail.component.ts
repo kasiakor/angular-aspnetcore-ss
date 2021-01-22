f42d52fcde4846fbe17bacc8880f0987fd88e88b
kasia.kordys <kasia.kordys@gmail.com>
2021-01-22 21:01:43 +0000

    use segment variable to request data from repository

--- ClientApp/src/app/structure/productDetail.component.ts
+++ ClientApp/src/app/structure/productDetail.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { Repository } from "../models/repository";
 import { Product } from "../models/product.model";
+import { Router, ActivatedRoute } from "@angular/router"
 
 @Component({
   selector: "product-detail",
@@ -8,9 +9,14 @@ import { Product } from "../models/product.model";
 })
 
 export class ProductDetailComponent {
-  constructor(private repo: Repository) { }
-    get product(): Product {
-      return this.repo.product;
+  constructor(private repo: Repository, router: Router, activeRoute: ActivatedRoute) { 
+  let id = Number.parseInt(activeRoute.snapshot.params["id"]);
+
+  if (id) {
+    this.repo.getProduct(id);
+  }
+  else {
+    router.navigateByUrl("/");
   }
 }
 
