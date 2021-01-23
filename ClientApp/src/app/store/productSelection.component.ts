d511ee3c44d2e3d8c42ef9067f41913ceb2392d3
kasia.kordys <kasia.kordys@gmail.com>
2021-01-23 23:43:50 +0000

    add structural/product selection component and template

---
+++ ClientApp/src/app/store/productSelection.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "store-products",
+  templateUrl: "productSelection.component.html"
+})
+
+export class ProductSelectionComponent {
+
+}
