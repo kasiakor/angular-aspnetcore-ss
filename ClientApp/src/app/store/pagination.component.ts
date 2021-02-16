5d404f660edbce4b51fc97f3a4e3d73f44b5190d
kasia.kordys <kasia.kordys@gmail.com>
2021-02-16 17:39:11 +0000

    update pagination component to provide support required for page navigation button

--- ClientApp/src/app/store/pagination.component.ts
+++ ClientApp/src/app/store/pagination.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { NavigationService } from "../models/navigation.service";
 
 @Component({
   selector: "store-pagination",
@@ -6,5 +7,16 @@ import { Component } from "@angular/core";
 })
 
 export class PaginationComponent {
+  constructor(public navigation: NavigationService) { }
+
+  get pages(): number[] {
+    if (this.navigation.productCount > 0) {
+      return Array(Math.ceil(this.navigation.productCount
+        / this.navigation.productsPerPage))
+        .fill(0).map((x, i) => i + 1);
+    } else {
+      return [];
+    }
+  }
 
 }
