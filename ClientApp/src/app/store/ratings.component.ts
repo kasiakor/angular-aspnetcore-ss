627668da9686997957596eecc0ced473d4ea397a
kasia.kordys <kasia.kordys@gmail.com>
2021-02-17 21:21:45 +0000

    add code to display ratings with style

--- ClientApp/src/app/store/ratings.component.ts
+++ ClientApp/src/app/store/ratings.component.ts
@@ -7,16 +7,19 @@ import { Product } from "../models/product.model";
 })
 
 export class RatingsComponent {
+  //allows component to receive data from the template in which it is applied to
   @Input()
   product: Product;
 
+  //generates an array of booleans
+  //three starts will generate true, true, true, false, false
   get stars(): boolean[] {
     if (this.product != null && this.product.ratings != null) {
       let total = this.product.ratings.map(r => r.stars)
         .reduce((prev, curr) => prev + curr, 0);
-      console.log("total rating:" + total )
+      console.log("total rating:" + total);
       let count = Math.round(total / this.product.ratings.length);
-      console.log("count: total/total.product.ratings.length" + count)
+      console.log("count: total/total.product.ratings.length" + count);
       return Array(5).fill(false).map((value, index) => {
         return index < count;
       });
