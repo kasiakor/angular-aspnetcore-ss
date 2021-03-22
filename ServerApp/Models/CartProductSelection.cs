418cd9a69d60ffb34470d98abe71a0dd13a6f274
kasia.kordys <kasia.kordys@gmail.com>
2021-03-22 21:45:27 +0000

    add model class to represent a cart in mvc project

---
+++ ServerApp/Models/CartProductSelection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerApp.Models
+{
+    public class CartProductSelection
+    {
+        public long productId { get; set; }
+        public string name { get; set; }
+        public decimal price { get; set; }
+        public int quantity { get; set; }
+    }
+}
