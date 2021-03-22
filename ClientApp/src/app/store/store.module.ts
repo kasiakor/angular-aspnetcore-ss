548265bf3c7b49f569ff9fdc2caec4491f3cdc5f
kasia.kordys <kasia.kordys@gmail.com>
2021-03-22 20:40:57 +0000

    register cartdetail component to store module

--- ClientApp/src/app/store/store.module.ts
+++ ClientApp/src/app/store/store.module.ts
@@ -6,11 +6,17 @@ import { PaginationComponent } from "./pagination.component";
 import { ProductListComponent } from "./productList.component";
 import { RatingsComponent } from "./ratings.component";
 import { ProductSelectionComponent } from "./productSelection.component";
+import { CartDetailComponent } from "./cartDetail.component";
+//to enable NgModule directive used in two-way data binding allowing the user to change the value in the input element
+import { FormsModule } from "@angular/forms";
+//required for button elements to which routerLink directived was applied.
+import { RouterModule } from "@angular/router";
+
 
 //configuration metadata
 @NgModule({
-  declarations: [CartSummaryComponent, CategoryFilterComponent, PaginationComponent, ProductListComponent, ProductSelectionComponent, RatingsComponent],
-  imports: [BrowserModule],
+  declarations: [CartSummaryComponent, CategoryFilterComponent, PaginationComponent, ProductListComponent, ProductSelectionComponent, RatingsComponent, CartDetailComponent],
+  imports: [BrowserModule, FormsModule, RouterModule],
   exports: [ProductSelectionComponent]
 })
 export class StoreModule {}
