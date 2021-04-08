d5df9ac7001ee2dbdf5f809e780e1b187f1494b8
kasia.kordys <kasia.kordys@gmail.com>
2021-04-08 20:27:58 +0100

    add checkout get and post methods to session values controller

--- ServerApp/Controllers/SessionValuesController.cs
+++ ServerApp/Controllers/SessionValuesController.cs
@@ -2,7 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using ServerApp.Models;
-
+using ServerApp.Models.BindingTargets;
+using System;
 
 namespace ServerApp.Controllers
 {
@@ -22,6 +23,27 @@ namespace ServerApp.Controllers
         {
             var jsonData = JsonConvert.SerializeObject(products);
             HttpContext.Session.SetString("cart", jsonData);
+        }
+
+        [HttpGet("checkout")]
+        public IActionResult GetCheckout()
+        {
+            //receives serialized data and returns it to the client
+            //getString reads data
+            //HttpContext.Session gives access to data
+            return Ok(HttpContext.Session.GetString("checkout"));
+        }
+
+        [HttpPost("checkout")]
+        public void StoreCheckout([FromBody] CheckoutState data)
+        {
+            Console.WriteLine(data);
+            //mvc model binder will read the data from body
+            //data is received as json from the client then parsed to create .net object
+            //then data is serialized as json again
+            var jsonData = JsonConvert.SerializeObject(data);
+            HttpContext.Session.SetString("checkout", jsonData);
+            //response: { "name":"Ewa Zak","address":"Flower Street","cardNumber":"grgggh","cardExpiry":"gggggg","cardSecurityCode":"t4ttt"}
         }
     }
 }
