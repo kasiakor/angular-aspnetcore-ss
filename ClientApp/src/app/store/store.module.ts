2df14d31ed9c16083a2e844beaf87780ecfbf42c
kasia.kordys <kasia.kordys@gmail.com>
2021-04-27 22:28:53 +0100

    register blazor loader component in store module file, add no-errors schema to prevent error for app element without corresponding component

--- ClientApp/src/app/store/store.module.ts
+++ ClientApp/src/app/store/store.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from "@angular/core";
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { CartSummaryComponent } from "./cartSummary.component";
 import { CategoryFilterComponent } from "./categoryFilter.component";
@@ -15,12 +15,14 @@ import { CheckoutDetailsComponent } from "./checkout/checkoutDetails.component";
 import { CheckoutPaymentComponent } from "./checkout/checkoutPayment.component";
 import { CheckoutSummaryComponent } from "./checkout/checkoutSummary.component";
 import { OrderConfirmationComponent } from "./checkout/orderConfirmation.component";
+import { BlazorLoader } from "./blazorLoader.component";
 
 
 //configuration metadata
 @NgModule({
-  declarations: [CartSummaryComponent, CategoryFilterComponent, PaginationComponent, ProductListComponent, ProductSelectionComponent, RatingsComponent, CartDetailComponent, CheckoutDetailsComponent, CheckoutPaymentComponent, CheckoutSummaryComponent, OrderConfirmationComponent],
+  declarations: [CartSummaryComponent, CategoryFilterComponent, PaginationComponent, ProductListComponent, ProductSelectionComponent, RatingsComponent, CartDetailComponent, CheckoutDetailsComponent, CheckoutPaymentComponent, CheckoutSummaryComponent, OrderConfirmationComponent, BlazorLoader],
   imports: [BrowserModule, FormsModule, RouterModule],
-  exports: [ProductSelectionComponent]
+  exports: [ProductSelectionComponent],
+  schemas: [NO_ERRORS_SCHEMA]
 })
 export class StoreModule {}
