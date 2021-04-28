6b7541043605174f4005e7a6ac3ddb95ded57983
kasia.kordys <kasia.kordys@gmail.com>
2021-04-28 20:52:02 +0100

    register externalservice in app module

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ModelModule } from "./models/model.module";
 //import { ProductDetailComponent } from "./structure/productDetail.component";
 import { FormsModule } from "@angular/forms";
 import { StoreModule } from "./store/store.module";
+import { ExternalService } from "./external.service";
 
 
 @NgModule({
@@ -21,7 +22,11 @@ import { StoreModule } from "./store/store.module";
     FormsModule,
     StoreModule
   ],
-  providers: [],
+  //A provider is an instruction to the Dependency Injection system on how to obtain a value for a dependency. Most of the time, these dependencies are services that you create and provide.
+  providers: [ExternalService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  //constructor ensures that externalservice object will be created to provide Blazor with a global function angular_searchProducts to invoke
+  constructor(external: ExternalService) { }
+  }
