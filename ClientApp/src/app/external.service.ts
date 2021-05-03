a344f462ed9201467df9481bdee395ad8ce3c353
kasia.kordys <kasia.kordys@gmail.com>
2021-05-03 22:58:54 +0100

    process events received from subject.next method in the external service

--- ClientApp/src/app/external.service.ts
+++ ClientApp/src/app/external.service.ts
@@ -3,14 +3,34 @@
 import { Injectable, NgZone } from "@angular/core";
 import { Repository } from "./models/repository";
 import { Product } from "./models/product.model";
+import { NavigationService } from "./models/navigation.service";
 
+
+interface DotnetInvokable {
+  invokeMethod<T>(methodName: string, ...args: any): T;
+  invokeMethodAsync<T>(methodName: string, ...args: any): Promise<T>;
+}
 
 @Injectable()
 export class ExternalService {
-  constructor(private repository: Repository, private zone: NgZone) {
+  private resetFunction: (msg: string) => {};
+  constructor(private repository: Repository,
+    private zone: NgZone,
+    private navService: NavigationService
+  ) {
     //window object provides access to js runtime's global scope
     //Blazor will use this global property to locate angular functionality
-      window["angular_searchProducts"] = this.doSearch.bind(this);
+    window["angular_searchProducts"] = this.doSearch.bind(this);
+
+    //angular creates global function to get an object from blazor app
+    //this object is accessed by invokeMethod and invokeMethodAsync
+    window["angular_receiveReference"] = this.receiveReference.bind(this);
+
+    navService.change.subscribe(update => {
+      if (this.resetFunction) {
+        this.resetFunction("Results reset");
+      }
+    });
   }
   async doSearch(searchTerm: string): Promise<Product[]> {
     //zone - reentering angular zone from the task that was executed outside angular zone
@@ -21,5 +41,10 @@ export class ExternalService {
       this.repository.filter.search = searchTerm;
       return (await this.repository.getProducts()).data;
     })
+  }
+  //navigation service sends an event through the observer, external service responds by inoking resetSearch method on the blazor object
+    receiveReference(target: DotnetInvokable) {
+        this.resetFunction =
+          (msg: string) => target.invokeMethod("resetSearch", msg);
   }
 }
